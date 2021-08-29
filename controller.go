4d4cb126e26d497f609670c988446aa701cc6c95
matt <mattia.fantoni@gmail.com>
2021-08-29 15:27:24 +0100

    removed error resource exists

--- controller.go
+++ controller.go
@@ -45,21 +45,15 @@ import (
 	listers "github.com/mattfanto/kafkaops-controller/pkg/generated/listers/kafkaopscontroller/v1alpha1"
 )
 
-const controllerAgentName = "sample-controller"
+const controllerAgentName = "kafkaops-controller"
 
 const (
 	// SuccessSynced is used as part of the Event 'reason' when a KafkaTopic is synced
 	SuccessSynced = "Synced"
-	// ErrResourceExists is used as part of the Event 'reason' when a KafkaTopic fails
-	// to sync due to a Deployment of the same name already existing.
-	ErrResourceExists = "ErrResourceExists"
 	// ErrResourceDeviated is used as part of the Event 'reason' when a KafkaTopic deviates
 	// from the original specification
 	ErrResourceDeviated = "ErrResourceDeviated"
 
-	// MessageResourceExists is the message used for Events when a resource
-	// fails to sync due to a Deployment already existing
-	MessageResourceExists = "Resource %q already exists and is not managed by KafkaTopic"
 	// MessageResourceSynced is the message used for an Event fired when a KafkaTopic
 	// is synced successfully
 	MessageResourceSynced = "KafkaTopic synced successfully"
